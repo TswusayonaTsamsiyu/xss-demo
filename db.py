f4678aa64a65adf8a37970b529774cafc202a5a1
Gabi Bronshtein <gabi.bronshtein@gmail.com>
2022-01-12 17:49:58 +0200

    another fix

--- db.py
+++ db.py
@@ -9,6 +9,11 @@ _PROD_DB = "postgres://ckzhgaznufpyio:65fe2044ba1f1e6b5fb93e83ebe43998ee4e1141a2
            "ec2-34-255-225-151.eu-west-1.compute.amazonaws.com:5432/d7f123k4q62nts"
 
 
+_INSERT_QUERY = 'INSERT INTO recipes (username, recipe, hidden) VALUES (?, ?, ?)'
+if _IS_PROD:
+    _INSERT_QUERY = _INSERT_QUERY.replace("?", "%s")
+
+
 def connect_db():
     db = psycopg2.connect(_PROD_DB) if _IS_PROD else sqlite3.connect(_DEV_DB)
     db.cursor().execute('CREATE TABLE IF NOT EXISTS recipes '
@@ -22,8 +27,7 @@ def connect_db():
 
 def add_recipe(username, recipe, hidden=False):
     db = connect_db()
-    db.cursor().execute('INSERT INTO recipes (username, recipe, hidden) '
-                        'VALUES (?, ?, ?)', (username, recipe, hidden))
+    db.cursor().execute(_INSERT_QUERY, (username, recipe, hidden))
     db.commit()
 
 
