58a85ec504d019a54ebe9a041f3e5df988239e96
Gabi Bronshtein <gabi.bronshtein@gmail.com>
2022-01-12 18:16:49 +0200

    so many fixes

--- db.py
+++ db.py
@@ -2,12 +2,10 @@ import os
 import sqlite3
 import psycopg2
 
-
 _IS_PROD = os.environ.get("mode", "dev") == "prod"
 _DEV_DB = "recipes.db"
 _PROD_DB = "postgres://ckzhgaznufpyio:65fe2044ba1f1e6b5fb93e83ebe43998ee4e1141a2e1a8d7533e83eab66bf94a@" \
            "ec2-34-255-225-151.eu-west-1.compute.amazonaws.com:5432/d7f123k4q62nts"
-
 
 _INSERT_QUERY = 'INSERT INTO recipes (username, recipe, hidden) VALUES (?, ?, ?)'
 if _IS_PROD:
@@ -37,12 +35,13 @@ def get_recipes(search_query, admin=False):
       FROM recipes
      WHERE ({admin} IS TRUE
             OR hidden IS FALSE)
-       AND ('{search_query}' IS NULL
+       AND ('{search_query}' = ''
             OR UPPER (username) = UPPER ('{search_query}')
             OR recipe LIKE '%{search_query}%')
     """
     try:
-        result = connect_db().cursor().execute(query)
-        return result.fetchall() if result else []
+        cursor = connect_db().cursor()
+        cursor.execute(query)
+        return cursor.fetchall()
     except sqlite3.OperationalError as error:
         raise Exception(f"Failed to execute {query}") from error
